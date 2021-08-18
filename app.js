45eec682f6200830fe75d8f7e2c45dd918f7156f
Meztli Valeriano Orozco <meztli.valeriano@gmail.com>
2021-08-17 22:58:56 -0500

    Se agrega express y dotenv

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hola Lunita");
+const express = require('express');
+
+const app = express();
+
+app.get('/',function(req,res) {res.send('Hola Lunita');});
+
+app.listen(3000);
+//se ejecuta con localhost:3000 en el navegador
